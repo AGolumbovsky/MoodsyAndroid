76067daa0105ffe6c61d2d9e6d556ae71ef04f92
AGolumbovsky <agolumbovsky@gmail.com>
2015-11-09 16:32:25 -0800

    added basic test animation

--- app/src/main/java/com/andriy/moodsy/MainActivity.java
+++ app/src/main/java/com/andriy/moodsy/MainActivity.java
@@ -2,8 +2,21 @@ package com.andriy.moodsy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void animateHappy (View view){
+
+        ImageView happy = (ImageView) findViewById(R.id.happy);
+        happy.animate().rotation(1800f).setDuration(2000);
+
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
